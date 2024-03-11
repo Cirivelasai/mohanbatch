5b1d8254b84b9eb498761902ca55a2fe09a6b0fd
Cirivelasai <162982050+Cirivelasai@users.noreply.github.com>
2024-03-11 16:01:57 +0530

    Add files via upload

---
+++ hari.py
@@ -0,0 +1,99 @@
+print("saikumar")
+''''
+1.)connecting is used for making documantation in a python file
+2.)when the line is connected ,that line will not be considered
+as part of the code.
+3.)when the program is executed,connected line will not be in the part
+of execution.
+----->NOTE:
+single line connecting can be done using # at the begining of the line
+eg:
+    #print("hello 1")
+import this
+print(this)
+'''
+import this
+a = 12
+print(a)
+'''
+ Bargained characters
+
+   controls and data structures-->ABC language
+   strings and its formats-->from C
+   Regular expression-->Pearl
+   Exception handling-->Module3
+   Magic methods from -->fortron
+#----> print(object,sep, end, file, flush)
+#print has 5 parameters
+
+#object --->the value to be printed is object
+#sep--->seperator
+#Eg
+#a = 8
+#b = 9
+#c = 89
+# print(a, b,c, sep='$')
+
+#end --->which is used to add the string at the end of the value,
+##It will not keep the command to stay in same line
+#Eg:
+#print(str(a), (b, end='%%')
+
+# ! ---->variables
+#?variables is a name which is used to identify the value of variable
+#print(str(c))
+
+#! Rules to dedfine variables
+#1.)variables have to start with only alphabets, underscores
+#2.)The char following the 1st char can be alphabets, numbers, underscores
+#3.)No spaces are allowed
+#4.)No special characters like !@#$%&*()+
+#5.)case sensitive
+#6.)keywords are not allowed as variables
+
+#a = 8
+#b = 9
+#print(a, b)
+
+#a=7
+#b=7
+
+#a= b = 7
+#print(a, b)
+
+#a=8, 7, 9.89, "hello"
+#print(a)
+#print(type(a))
+
+a, b = 78
+print(a, b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
